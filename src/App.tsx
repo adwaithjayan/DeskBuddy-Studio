172c1491286abf1093ca7b16c3fa2c930b024b29
adwaithjayan <aj007boss@gmail.com>
2026-07-02 20:34:35 +0530

    feat(studio): initialize DeskBuddy Studio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="bg-amber-600 text-9xl">Hello World
+    <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
